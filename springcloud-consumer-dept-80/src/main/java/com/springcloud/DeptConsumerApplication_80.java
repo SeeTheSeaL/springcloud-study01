5e76161c2737f0a58e1b8867f336282cebe5ebaf
lee <L18249290950@126.com>
2021-02-01 22:14:11 +0800

    commit1

--- springcloud-consumer-dept-80/src/main/java/com/springcloud/DeptConsumerApplication_80.java
+++ springcloud-consumer-dept-80/src/main/java/com/springcloud/DeptConsumerApplication_80.java
@@ -2,12 +2,16 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @author lee
  * @date 2020/10/6 - 4:46 下午
  */
 @SpringBootApplication
+@EnableEurekaClient //开启Eureka
+@RibbonClient(name = "SPRINGCLOUD-PROVIDER-DEPT")
 public class DeptConsumerApplication_80 {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumerApplication_80.class,args);
